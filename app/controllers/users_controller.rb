4b64c72864b71597cffc26cf9d0e878cf161cbbf
yutokunaga <yutokunaga4107@gmail.com>
2019-03-10 22:43:51 +0900

    created routes and add new controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,44 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def login
+
+  end
+
+  def login_form
+
+  end
+
+  def logout
+
+  end
+
+  def likes
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  post 'likes/:post_id/create', to: 'likes#create'
+  post 'likes/:post_id/destroy', to: 'likes#destroy'
 
-  get '/posts/index', to:'posts#index'
+  post 'users/:id/update', to:'users#update'
+  post 'users/:id/edit', to:'users#edit'
+  post 'users/create', to:'users#create'
+  get 'signup', to:'users#signup'
+  get 'users/index', to:'users#index'
+  get 'users/:id', to:'users#show'
+  post 'login', to:'users#login'
+  post 'logout', to:'users#logout'
+  get 'login', to:'users#login_form'
+  get 'users/:id/likes', to:'users#likes'
+
+  get 'posts/index', to:'posts#index'
+  get 'posts/new', to:'posts#new'
+  get 'posts/:id', to:'posts#show'
+  post 'posts/create', to:'posts#create'
+  post 'posts/comment', to:'posts#comment'
+  get 'posts/:id/edit', to:'posts#edit'
+  get 'posts/:id/update', to:'posts#update'
+  get 'posts/:id/destroy', to:'posts#destroy'
+
+
 
   get '/', to: 'home#top'
 end
